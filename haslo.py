7c0a71f9fd3c3e1a70c4620fe6ec54b48ba360c7
Maciej Dems <maciej.dems@p.lodz.pl>
2020-10-16 09:08:30 +0200

    Szablon ćwiczenia

---
+++ haslo.py
@@ -0,0 +1,21 @@
+# coding: utf8
+"""
+Należy napisać program, który poprosi użytkownika o wprowadzenie hasła,
+a następnie wydrukuje dokładnie jeden z następujących komunikatów:
+"Hasło jest bezpieczne." bądź "Hasło jest niebezpieczne!".
+Bezpieczne hasło musi spełniać następujące warunki:
+
+* posiadać przynajmniej jedną małą literę,
+* posiadać przynajmniej jedna wielką literę,
+* posiadać przynajmniej jedną cyfrę.
+
+Przykłady:
+
+Podaj hasło: Katar7yna
+Hasło jest bezpieczne.
+
+Podaj hasło: katastrofa01
+Hasło jest niebezpieczne!
+"""
+
+# tutaj należy umieścić rozwiązanie
---
+++ trojkat1.py
@@ -0,0 +1,16 @@
+# coding: utf8
+"""
+Należy napisać program, który zapyta użytkownika o liczbę całkowitą (wielkość
+trójkąta) a następnie wydrukuje trójkąt prostokątny (z kątem prostym w lewym
+dolnym rogu) złożony ze znaków `*` o zadanej wielkości.
+
+Na przykład:
+
+Podaj wielkość trójkąta: 4
+*
+**
+***
+****
+"""
+
+# tutaj należy umieścić rozwiązanie
